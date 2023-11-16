126b3b164652688c7514ebcd1a2f2f67bb42c467
SemNik <142649558+SemNik88@users.noreply.github.com>
2023-11-16 06:56:21 +0300

    Update FreelancerService.java

--- src/main/java/ru/netology/services/FreelancerService.java
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -4,13 +4,12 @@ public class FreelancerService {
     public int calculateRestMonths(int income, int expenses, int threshold) {
         int currentMoney = 0;
         int restMonths = 0;
-
+        int expences = 0;
         for (int i = 1; i <= 12; i++) {
             if (currentMoney < threshold) {
                 // Работаем
-                int expences = 0;
                 currentMoney += income - expences;
-                currentMoney -= expenses;
+                currentMoney -= expenses;//когда дохода нет, а жить нужно, без нее смотрите скрин-шот с ошибкой
             } else {
                 // Отдыхаем
                 currentMoney = (currentMoney - expenses) / 3;
